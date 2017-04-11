1701edc0f2770f04dd80e4ea02d0b2528525d8b5
Subhojit Paul <subhojitpaul21@gmail.com>
2017-04-11 11:31:37 +0530

    Code standard check in CI

---
+++ drupal_ti/script/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Add an optional statement to see that this is running in Travis CI.
+echo "running drupal_ti/script/script.sh"
+
+set -e $DRUPAL_TI_DEBUG
+
+# Ensure the right Drupal version is installed.
+# Note: This function is re-entrant.
+drupal_ti_ensure_drupal
+
+# Check Drupal code standard.
+phpcs -i
+phpcs --standard=Drupal --extensions=php,module,inc,install,profile,theme,css,info,txt,md,js includes js tests/features tests/modules theme listjs.info listjs.module
+phpcs --standard=DrupalPractice  --extensions=php,module,inc,install,profile,theme,css,info,txt,md,js includes js tests/features tests/modules theme listjs.info listjs.module
